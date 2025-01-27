09e58c948b056e644f79d19570656d7535a8ddd6
Gagan Sai Sri Ram <saisriram.k21@iiits.in>
2025-01-27 12:00:48 +0530

    Added City Repository Layer

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -2,15 +2,18 @@ const {City} = require('../models/index');
 
 class CityRepository {
     
-     async createCity({name}){
+     async createCity({name}){  // {name:"New dehli"} -> destructing an js object !! 
           try {
 
-            const city = await City.create({name});
+            const city = await City.create({
+              name
+            });
 
             return city;
                
           }catch(error){
 
+             console.log("Something went wrong in the repository layer");
              throw {error};
 
           }
@@ -22,13 +25,50 @@ class CityRepository {
                   where : {
                     id:cityId,
                   }
-              })
+              });
+
+              return true;
 
           }catch(error){
+
+            console.log("Something went wrong in the repository layer");
 
               throw {error};
               
           }
+     }
+
+     async updateCity(cityId,data){
+        
+      try{
+          const city = await City.update(data , {
+              where : {
+                 id:cityId
+              }
+          });
+
+          return city;
+      }catch(error){
+        console.log("Something went wrong in the repository layer");
+
+        throw {error};
+      }
+
+
+
+     }
+
+     async getCity(cityId){
+        
+      try {
+          const city = await City.findByPk(cityId);
+          return city;
+      }catch(error){
+        console.log("Something went wrong in the repository layer");
+
+        throw {error};
+      }
+
      }
 }
 
