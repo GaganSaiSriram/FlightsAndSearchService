84b624c578b315afbcf2e6107d3ab37a47255425
Gagan Sai Sri Ram <saisriram.k21@iiits.in>
2025-01-27 12:00:48 +0530

    Added City Repository Layer

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended : true}));
     
-app.listen(PORT , function (){
+app.listen(PORT , async function (){
+
+    
     
     console.log(`Server started at PORT ${PORT}`);
 })
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -2,15 +2,18 @@ const {City} = require('../models/index');
 
 class CityRepository {
     
-     async createCity({name}){
+     async createCity({name}){  // {name:"New dehli"} -> destructing an js object !! 
           try {
 
-            const city = await City.create({name});
+            const city = await City.create({
+              name
+            });
 
             return city;
                
           }catch(error){
 
+             console.log("Something went wrong in the repository layer");
              throw {error};
 
           }
@@ -26,9 +29,44 @@ class CityRepository {
 
           }catch(error){
 
+            console.log("Something went wrong in the repository layer");
+
               throw {error};
               
           }
+     }
+
+     async updateCity(cityId,data){
+        
+      try{
+          const city = await City.update(data , {
+              where : {
+                 id:cityId
+              }
+          });
+
+          return city;
+      }catch(error){
+        console.log("Something went wrong in the repository layer");
+
+        throw {error};
+      }
+
+
+
+     }
+
+     async getCity(cityId){
+        
+      try {
+          const city = await City.findByPk(cityId);
+          return city;
+      }catch(error){
+        console.log("Something went wrong in the repository layer");
+
+        throw {error};
+      }
+
      }
 }
 
