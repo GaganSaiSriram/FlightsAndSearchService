c9706eb414e28e88495ef755fa78d58637774815
Gagan Sai Sri Ram <saisriram.k21@iiits.in>
2025-01-28 21:26:48 +0530

    Added seeders and some other things

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const bodyParser  = require('body-parser');
 
 const apiRoutes = require('./routes/index');
 
-const {PORT} = require('./config/serverConfig');
+const {PORT , SYNC_DB} = require('./config/serverConfig');
+
+const db = require('./models/index');
+
+const sequelize = require('sequelize');
 
 const setupAndStartServer = async function (){
     // create the express object
@@ -15,12 +19,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.use('/api' , apiRoutes)
-    
+
 app.listen(PORT , async function (){
 
-    
-    
-    console.log(`Server started at PORT ${PORT}`);
+console.log(`Server started at PORT ${PORT}`);
+
+
+
+  if(SYNC_DB == 'true'){
+    await db.sequelize.sync({alter:true})
+  }
+
 })
 
 
